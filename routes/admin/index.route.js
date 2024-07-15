7f0abd4bb5ae3a83bbe19a024daa919ae541ecda
DangThanhMai <dangmai19112016@gmail.com>
2024-07-15 12:34:53 +0700

    Create page Product-category

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,8 +1,10 @@
 const dashboardRoute=require("./dashboard.route.js")
 const productRoute=require("./product.route.js")
+const productCategoryRoute=require("./product-category.route.js")
 const systemConfig=require("../../config/system.js")
 module.exports=(app)=>{
     const PATH=systemConfig.prefixAdmin
     app.use(PATH+"/dashboard",dashboardRoute)
     app.use(PATH+"/products",productRoute)
+    app.use(PATH+"/products-category",productCategoryRoute)
 }
