ad84c38c7a3c53c5398f1256f4e1328268d4d017
Maiz07042004 <dangmai19112016@gmail.com>
2024-08-04 19:59:42 +0700

    Hiển thị danh sách sản phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -4,14 +4,24 @@ const createTreeHelpers=require("../../helpers/createTree")
 const productHelpers=require("../../helpers/product")
 // [GET]/
 module.exports.index=async(req,res)=>{
-    const products=await Product.find({
+    // Lấy ra sản phẩm nổi bật
+    const productsFeatured=await Product.find({
         deleted:false,
         status:"active",
         featured: "1"
     }).limit(6)
-    const newProducts=productHelpers.priceNewProducts(products)
+    const newProductsFeatured=productHelpers.priceNewProducts(productsFeatured)
+    // Hết lấy ra sản phẩm nổi bật
+
+    // Hiển thị danh sách sản phẩm mới nhất
+    const productsNew=await Product.find({
+        deleted:false,
+        status:"active"
+    }).sort({position: "desc"}).limit(6)
+    const newProductsNew=productHelpers.priceNewProducts(productsNew)
     res.render("client/pages/home/index",{
         pageTitle:"Trang chủ",
-        productsFeatured:newProducts
+        productsFeatured:newProductsFeatured,
+        productsNew:newProductsNew
     });
 }
