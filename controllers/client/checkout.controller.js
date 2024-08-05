5a7e93fd6c753e49fbd0f4c7ca20b419bb0fec3c
Maiz07042004 <dangmai19112016@gmail.com>
2024-08-05 21:58:49 +0700

    Làm giao diện trang checkout

---
+++ controllers/client/checkout.controller.js
@@ -0,0 +1,27 @@
+const Cart=require("../../models/cart.model");
+const Product=require("../../models/product.model");
+const productsHelper=require("../../helpers/products")
+
+// [GET]/checkout/
+module.exports.index=async(req,res)=>{
+    const cartId=req.cookies.cartId;
+    const cart=await Cart.findOne({
+        _id:cartId
+    })
+    if(cart.products.length>0){
+        for (const item of cart.products) {
+            const productId=item.product_id
+            const productInfo=await Product.findOne({
+                _id:productId
+            })
+            productInfo.priceNew=productsHelper.priceNewProduct(productInfo)
+            item.totalPrice=productInfo.priceNew*item.quantity
+            item.productInfo=productInfo
+        }
+        cart.totalPrice=cart.products.reduce((sum,item)=>sum+item.totalPrice,0)
+    }
+    res.render("client/pages/checkout/index",{
+        pageTitle:"Đặt hàng",
+        cartDetail:cart
+    })
+}
