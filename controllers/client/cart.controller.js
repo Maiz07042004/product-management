fd847275a5ce44b1b74bf13a4342ac163441d5e3
Maiz07042004 <dangmai19112016@gmail.com>
2024-08-05 20:21:46 +0700

    Làm trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,5 +1,31 @@
 const Cart=require("../../models/cart.model");
-const product=require("../../models/product.model")
+const Product=require("../../models/product.model")
+const productsHelper=require("../../helpers/products")
+
+// [GET]/cart
+module.exports.index=async(req,res)=>{
+    const cartId=req.cookies.cartId;
+    const cart=await Cart.findOne({
+        _id:cartId
+    })
+    if(cart.products.length>0){
+        for (const item of cart.products) {
+            const productId=item.product_id
+            const productInfo=await Product.findOne({
+                _id:productId
+            })
+            productInfo.priceNew=productsHelper.priceNewProduct(productInfo)
+            item.totalPrice=productInfo.priceNew*item.quantity
+            item.productInfo=productInfo
+        }
+        cart.totalPrice=cart.products.reduce((sum,item)=>sum+item.totalPrice,0)
+    }
+    
+    res.render("client/pages/cart/index.pug",{
+        pageTitle:"Giỏ hàng",
+        cartDetail:cart
+    })
+}
 
 // [POST]/cart/add/:productId
 module.exports.addPost=async(req,res)=>{
