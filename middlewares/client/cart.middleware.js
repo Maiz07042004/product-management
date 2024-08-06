7729f1509c463621f288c7fb7f4d2eefd05b4c7c
Maiz07042004 <dangmai19112016@gmail.com>
2024-08-06 16:53:10 +0700

    Đăng ký tài khoản

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -1,20 +1,46 @@
-const Cart=require("../../models/cart.model")
+const Cart = require("../../models/cart.model");
 
-module.exports.cartId=async(req,res,next)=>{
-    if(!req.cookies.cartId){
-        const cart=new Cart()
-        await cart.save()
-        const expiresTime=1000*60*60*24*365
+module.exports.cartId = async (req, res, next) => {
+    try {
+        if (!req.cookies.cartId) {
+            const cart = new Cart();
+            await cart.save();
+            const expiresTime = 1000 * 60 * 60 * 24 * 365;
 
-        res.cookie("cartId",cart.id,{
-            expires: new Date(Date.now()+expiresTime)
-        });
-    }else{
-        const cart=await Cart.findOne({
-            _id:req.cookies.cartId
-        })
-        cart.totalQuantity=cart.products.reduce((sum,item)=>sum+item.quantity,0)
-        res.locals.miniCart=cart
+            res.cookie("cartId", cart.id, {
+                expires: new Date(Date.now() + expiresTime),
+                httpOnly: true, // Thêm để tăng tính bảo mật
+                secure: process.env.NODE_ENV === 'production' // Thêm để cookie an toàn trong môi trường sản xuất
+            });
+            res.locals.miniCart = cart; // Đảm bảo res.locals.miniCart luôn được thiết lập
+        } else {
+            const cart = await Cart.findOne({ _id: req.cookies.cartId });
+
+            if (cart) {
+                if (Array.isArray(cart.products)) {
+                    cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0);
+                } else {
+                    cart.totalQuantity = 0;
+                }
+                res.locals.miniCart = cart;
+            } else {
+                // Nếu không tìm thấy giỏ hàng, tạo một giỏ hàng mới
+                const newCart = new Cart();
+                await newCart.save();
+                const expiresTime = 1000 * 60 * 60 * 24 * 365;
+
+                res.cookie("cartId", newCart.id, {
+                    expires: new Date(Date.now() + expiresTime),
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production'
+                });
+                res.locals.miniCart = newCart;
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        next(error); // Chuyển lỗi đến middleware tiếp theo
     }
-    next()
-}
+
+    next();
+};
--- validates/admin/product.validate.js
+++ validates/admin/product.validate.js
@@ -4,5 +4,11 @@ module.exports.createPost=(req,res,next)=>{
         res.redirect("back");
         return;
     }
+
+    if(req.body.title.length<5){
+        req.flash("error","Tiêu đề dài tối thiểu là 5 ký tự!")
+        res.redirect("back");
+        return;
+    }
     next();
 }
