28e3a6648d5769c01b55d89df7eb217684d1c0f7
Maiz07042004 <dangmai19112016@gmail.com>
2024-08-08 23:18:39 +0700

    Hiển thị data realtime

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -3,14 +3,23 @@ const User=require("../../models/user.model")
 
 module.exports.index=async(req,res)=>{
     const userId=res.locals.user.id
+    const fullName=res.locals.user.fullName
     // Socket.io
     _io.once('connection', (socket) => {
         socket.on("CLIENT_SEND_MESSAGE", async(content) => {
-          const chat=new Chat({
-            user_id: userId,
-            content: content
-          })
-          await chat.save()
+        // Lưu vào database
+            const chat=new Chat({
+                user_id: userId,
+                content: content
+            })
+            await chat.save()
+
+            // Trả data về client
+            _io.emit("SEVER_RETURN_MESSAGE",{
+                userId:userId,
+                fullName:fullName,
+                content:content
+            })
         });
       });
     // End socket.io
--- public/js/chat.js
+++ public/js/chat.js
@@ -12,3 +12,25 @@ if(formSendData){
 }
 
 // END CLIENT_SEND_MESSAGE
+
+// SEVER_RETURN_MESSAGE
+socket.on("SEVER_RETURN_MESSAGE",(data)=>{
+    const body=document.querySelector(".chat .inner-body")
+    const div=document.createElement("div")
+    const myId=document.querySelector("[my-id]").getAttribute("my-id")
+    let fullName=""
+    if(myId==data.userId){
+        div.classList.add("inner-outgoing")
+    }else{
+        div.classList.add("inner-incoming")
+        fullName=`<div class="inner-name">${data.fullName}</div>`
+    }
+    div.innerHTML=`
+        ${fullName}
+        <div class="inner-content">${data.content}</div>
+    `
+    body.appendChild(div)
+    body.scrollTop = body.scrollHeight;
+
+})
+// END SEVER_RETURN_MESSAGE
